743653a6325b2d36e299f61719976d6539862f55
juan ignacio barraza <juanignaciobarraza99@gmail.com>
2024-06-10 16:58:48 -0300

    login1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { AuthService } from './service/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +10,21 @@ import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
   imports: [IonApp, IonRouterOutlet],
 })
 export class AppComponent {
-  constructor() {}
+  authService = inject(AuthService);
+  router = inject(Router);
+
+  ngOnInit(): void {
+    this.router.navigateByUrl('/');
+
+    this.authService.user$.subscribe((user) => {
+      if (user) {
+        this.authService.currentUserSig.set({
+          email: user.email!,
+          username: user.displayName!,
+        });
+      } else {
+        this.authService.currentUserSig.set(null);
+      }
+    });
+  }
 }
---
+++ src/app/page/home/home.page.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonButton,
+  IonButtons,
+  IonIcon,
+  IonNavLink,
+  IonAlert,
+} from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/service/auth.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+  standalone: true,
+  imports: [
+    IonAlert,
+    IonNavLink,
+    IonIcon,
+    IonButtons,
+    IonButton,
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+  ],
+})
+export class HomePage implements OnInit {
+  //Injecciones
+  auth = inject(AuthService);
+  authService = inject(AuthService);
+  router = inject(Router);
+
+  constructor() {}
+
+  //Form - Alert
+
+  //Ruteo
+  goRegister() {
+    this.router.navigateByUrl('/register');
+  }
+  goLogin() {
+    this.router.navigateByUrl('/login');
+  }
+  goHome() {
+    this.router.navigateByUrl('/home');
+  }
+  logout() {
+    this.authService.logout();
+    this.router.navigateByUrl('/login');
+  }
+  //Init - Destroy
+  ngOnInit() {}
+}
