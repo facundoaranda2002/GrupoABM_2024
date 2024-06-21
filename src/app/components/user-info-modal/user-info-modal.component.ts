1273f4067e8332d9fe2f945e48300cfdf651547f
JavierMendozaBenitez <javier.mendoza.benitez@gmail.com>
2024-06-21 17:20:52 -0300

    Agregue Ir a tu mesa

--- src/app/components/user-info-modal/user-info-modal.component.ts
+++ src/app/components/user-info-modal/user-info-modal.component.ts
@@ -8,45 +8,42 @@ import emailjs from '@emailjs/browser';
   templateUrl: './user-info-modal.component.html',
   styleUrls: ['./user-info-modal.component.scss'],
 })
-export class UserInfoModalComponent  implements OnInit {
+export class UserInfoModalComponent implements OnInit {
 
-  @Input() user : any;
-  mensajes : any;
-  constructor(private modalController : ModalController, private data : ClienteService) { }
+  @Input() user: any;
+  mensajes: any;
+  constructor(private modalController: ModalController, private data: ClienteService) { }
 
   ngOnInit() {
     this.mensajes = ["Ha sido aceptado por un supervisor. Ahora su cuenta se encuentra registrada. Intente ingresar",
-  "Lamentamos informarle que un supervisor a decidido rechazar su cuenta."]
+      "Lamentamos informarle que un supervisor a decidido rechazar su cuenta."]
   }
 
-  public async onDecideClick(validated : boolean | null)
-  {
-    if(validated != null)
-    {
+  public async onDecideClick(validated: boolean | null) {
+    if (validated != null) {
       let userUID = await this.data.GetUserUIDByUserEmail(this.user.mail);
       let estaValidado;
-      if(validated)
-      estaValidado = 'aceptado';
+      if (validated)
+        estaValidado = 'aceptado';
       else
-      estaValidado = 'rechazado';
-      if(userUID!==null)
-      this.data.actualizarEstadoCliente(userUID, estaValidado)  
+        estaValidado = 'rechazado';
+      if (userUID !== null)
+        this.data.actualizarEstadoCliente(userUID, estaValidado)
       let mensaje = "";
-      if(estaValidado === 'aceptado')
-      {
+      if (estaValidado === 'aceptado') {
         mensaje = this.mensajes[0];
-      }    
-      else{
+      }
+      else {
         mensaje = this.mensajes[1];
       }
       emailjs.init("zy9ZbGaMjTB6ScJGM");
-      await emailjs.send("service_ldf8519","template_8roq68a", {
+      await emailjs.send("service_ldf8519", "template_8roq68a", {
         from_name: 'Grupo ABM',
-        to_name: this.user.nombre,        
+        to_name: this.user.nombre,
         to_email: this.user.mail,
         subject: 'Estado de tu cuenta para el restaurante del grupo ABM',
         message: mensaje,
-      }); 
+      });
     }
 
     this.modalController.dismiss();
